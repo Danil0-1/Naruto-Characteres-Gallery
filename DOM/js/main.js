b1641a4963fe1c20a5d36d5d5ecea33dc28c55df
Danil0-1 <danilomuskus1@gmail.com>
2025-04-10 00:10:45 -0500

    feat: :sparkles: Implementacion de la segunda imagen

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -19,3 +19,18 @@ let titulo1 = document.getElementById("cardtitle1")
 let imagen1 = document.getElementById("cardimage1")
 let clan1 = document.getElementById("cardhead1")
 let habilidad1 = document.getElementById("carddescription1")
+
+imagen1.src = a.images[0]
+titulo1.innerHTML = a.name
+clan1.innerHTML = (`Clan: ${a.personal.clan} Aldea: ${a.personal.affiliation[0]}`)
+habilidad1.innerHTML = (`Habilidad: ${a.jutsu[52]}`)
+
+let titulo2 = document.getElementById("cardtitle2")
+let imagen2 = document.getElementById("cardimage2")
+let clan2 = document.getElementById("cardhead2")
+let habilidad2 = document.getElementById("carddescription2")
+
+imagen2.src = b.images[0]
+titulo2.innerHTML = b.name
+clan2.innerHTML = (`Clan: ${b.personal.clan} Aldea: ${b.personal.affiliation[0]}`)
+habilidad2.innerHTML = (`Habilidad: ${b.jutsu[1]}`)
